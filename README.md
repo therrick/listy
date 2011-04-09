af219155f83ea6b16c65fedf75d8bef5cae32da7
therrick <therrick@gmail.com>
2011-04-09 13:46:14 -0400

    shell rails app

---
+++ README.md
@@ -0,0 +1,1 @@
+listy is a shopping list app, written in rails 3 and ruby 1.9.2.  It is optimized for simple sharing across smartphones and PCs.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listy::Application.config.secret_token = '76b8f7c156b73fca334746783eaae8ed75bbf6adf8dca81ad74cf2d897d8c5908a7829bd843160764b5cdd8a79afd5184111b5ef9e43da53b2a7781070e2ab90'
