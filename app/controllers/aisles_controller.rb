4e8b0a0b0f7aa123dc632c6d6b89715c305ce7de
therrick <therrick@gmail.com>
2011-04-18 21:16:50 -0400

    sorting gets out of whack when aisle is deleted

--- app/controllers/aisles_controller.rb
+++ app/controllers/aisles_controller.rb
@@ -31,18 +31,29 @@ class AislesController < ApplicationController
     end
     
     def move_up
+      # TODO: what a freakin mess.  there's gotta be a more efficient way to do this
       @store = current_user.stores.find(params[:store_id])
       aisles = @store.aisles
+
+      # first, compact everything back down to 1-n in case a delete or something got it out of whack
+      aisles.sort! { |a,b| a.position <=> b.position }
+      aisles.each_with_index do |aisle, i|
+        aisle.position = i + 1
+        print ("position: #{aisle.position}\n")
+        aisle.save!
+      end
+
       @aisle = aisles.find(params[:id])
       original_position = @aisle.position
+      print "original_position: #{original_position}\n"
       if original_position > 1
         @aisle.position -= 1
-        @aisle.save
+        @aisle.save!
         aisles.each do |aisle|
           aisle.position += 1 if aisle != @aisle &&
             aisle.position >= @aisle.position &&
             aisle.position <= original_position
-          aisle.save
+          aisle.save!
         end
       end
       redirect_to(store_aisles_url(@store_id))
@@ -53,7 +64,8 @@ class AislesController < ApplicationController
       aisles = @store.aisles
       aisles.each do |aisle|
         aisle.position = params['aisle'].index(aisle.id.to_s) + 1
-        aisle.save
+        print ("position: #{aisle.position}\n")
+        aisle.save!
       end
       render :nothing => true
     end
