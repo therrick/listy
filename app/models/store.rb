ac0fea7c66c33c73b658f1a2a01d72d0dd996f06
therrick <therrick@gmail.com>
2011-04-22 14:51:30 -0400

    extend 'remember me' time period; fix some tests

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,5 @@
 class Store < ActiveRecord::Base
+  belongs_to :user
   has_many :items
   has_many :aisles, :order => "position"
 end
--- test/functional/items_controller_test.rb
+++ test/functional/items_controller_test.rb
@@ -1,47 +1,55 @@
 require 'test_helper'
 
 class ItemsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers  
+  
   setup do
     @item = items(:one)
+    assert_not_nil @item
+    @user = users(:one)
+    assert_not_nil @user
+    sign_in @user
+    @store = @user.stores.first
+    assert_not_nil @store
   end
 
   test "should get index" do
-    get :index
+    get :index, :store_id => @store.id
     assert_response :success
     assert_not_nil assigns(:items)
   end
 
   test "should get new" do
-    get :new
+    get :new, :store_id => @store.id
     assert_response :success
   end
 
   test "should create item" do
     assert_difference('Item.count') do
-      post :create, :item => @item.attributes
+      post :create, :item => @item.attributes, :store_id => @store.id
     end
 
     assert_redirected_to item_path(assigns(:item))
   end
 
   test "should show item" do
-    get :show, :id => @item.to_param
+    get :show, :id => @item.to_param, :store_id => @store.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => @item.to_param
+    get :edit, :id => @item.to_param, :store_id => @store.id
     assert_response :success
   end
 
   test "should update item" do
-    put :update, :id => @item.to_param, :item => @item.attributes
+    put :update, :id => @item.to_param, :item => @item.attributes, :store_id => @store.id
     assert_redirected_to item_path(assigns(:item))
   end
 
   test "should destroy item" do
     assert_difference('Item.count', -1) do
-      delete :destroy, :id => @item.to_param
+      delete :destroy, :id => @item.to_param, :store_id => @store.id
     end
 
     assert_redirected_to items_path
